713a129c92900a011291490af28c90393c3a6bd8
Reynaldo Guarachi <jesus.guarachi@icloud.com>
2024-06-16 04:38:22 -0400

    primer componente

--- pomodoro/App.js
+++ pomodoro/App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {Header} from './src/components/Header';
 
+const colors = ["#F7DC8F", "#A2D9CE","D7BDE2"];
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25*60);
+  const [currentTime, setCurrentTime] = useState("POMO"|"SHORT"|"BREAK");
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ paddingTop: Platform.OS === "android" && 30}}>
-      <Text style={styles.text} >Pomodoro</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <Text style={styles.text} >{time}</Text>
+     
       </View>
     </SafeAreaView>
   );
---
+++ pomodoro/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { View , Text, TouchableOpacity, StyleSheet} from "react-native";
+
+export const Header=()=>{
+    return(
+        <View>
+            <Text>Pomodoro</Text>
+        </View>
+    )
+}
